f364ef57c3061a1d2b5f83e9cc70de4077674e2d
tai-phan <phantuantai7293@gmail.com>
2022-10-15 12:22:15 +0700

    small edit

--- client/src/containers/HomeTemplate/components/TopHeader.jsx
+++ client/src/containers/HomeTemplate/components/TopHeader.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Search, FavoriteBorder, PermIdentity } from "@mui/icons-material";
+import logo from "../../../assets/logo.png";
 
 function TopHeader() {
-    return <div className="topheader_container"></div>;
+    return (
+        <div className="topheader_container">
+            <div className="topheader_logo">
+                <Link to="/">
+                    <img src={logo} alt="logo" />
+                </Link>
+            </div>
+            <div className="topheader_inputs"></div>
+            <div className="topheader_user"></div>
+        </div>
+    );
 }
 
 export default TopHeader;
